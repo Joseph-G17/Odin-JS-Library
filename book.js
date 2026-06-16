fd7d8ebec1cc846edd8623e974d6c3528b063e0c
Joseph Guzman <guzman.josephg@gmail.com>
2026-06-16 16:54:00 -0500

    first commit

---
+++ book.js
@@ -0,0 +1,16 @@
+class Book {
+    constructor(name, author, year) {
+        this.name = name;
+	    this.author = author;
+	    this.year = year;
+    }
+	
+	info() {
+		return `${this.name} by ${this.author}, ${this.year}`;
+	}
+}
+
+//MAIN/CONSOLE:
+
+const user_book = new Book("A Christmas Carol", "Charles Dickens", 1843);
+console.log(user_book.info());
